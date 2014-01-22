0bdb270c707d9ef46776e4a1a81817413b894359
Martin Jul <martin@mjul.com>
2014-01-22 17:50:07 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to opencvlab
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/opencvlab/core_test.clj
@@ -0,0 +1,7 @@
+(ns opencvlab.core-test
+  (:require [clojure.test :refer :all]
+            [opencvlab.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
